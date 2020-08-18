34ec8d2c0db0eb50662383dfe79a89a3ccf39def
Roy Prins <roy.prins@royprins-ltm7.internal.salesforce.com>
2020-08-18 10:45:32 +0200

    Updated Mule 4 runtime versions

--- mule4/CE/build.sh
+++ mule4/CE/build.sh
@@ -21,7 +21,7 @@ fi
 
 #----- Set runtime version
 if [[ -z $2 ]] ; then
-   RUNTIME_VERSION=4.1.1
+   RUNTIME_VERSION=4.2.1
 else
    RUNTIME_VERSION=$2
 fi
--- mule4/EE-license/build.sh
+++ mule4/EE-license/build.sh
@@ -21,7 +21,7 @@ fi
 
 #----- Set runtime version
 if [[ -z $2 ]] ; then
-   RUNTIME_VERSION=4.2.0
+   RUNTIME_VERSION=4.3.0
 else
    RUNTIME_VERSION=$2
 fi
