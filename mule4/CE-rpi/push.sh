4758f2e6b32b81870fabd6eacc615609ea7ed231
Roy Prins <rajprins@gmail.com>
2024-03-19 16:38:19 +0100

    Bumped Mule 4 runtime version

--- mule4/CE-rpi/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker tag mule4-ce-rpi rprins/mule4-ce-rpi:latest
-docker push rprins/mule4-ce-rpi:latest
--- mule4/CE/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker tag mule4-ce rprins/mule4-ce:latest
-docker push rprins/mule4-ce:latest
